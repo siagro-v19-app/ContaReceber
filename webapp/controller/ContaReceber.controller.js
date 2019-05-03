6dbd16f815f994c4a5a3e1cab37308fce6541710
matusca96 <gomesloud@gmail.com>
2019-05-03 10:49:52 -0300

    Atualização.

--- webapp/controller/ContaReceber.controller.js
+++ webapp/controller/ContaReceber.controller.js
@@ -15,19 +15,24 @@ sap.ui.define([
 			this.getOwnerComponent().setModel(oParamModel, "parametros");
 			this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass());
 			
-			this.getModel().attachMetadataLoaded(function(){
-				var oFilter = new Filter("Empresa", FilterOperator.EQ, Session.get("EMPRESA_ID"));
-				var oView = this.getView();
-				var oTable = oView.byId("tableContaReceber");
-				var oColumn = oView.byId("columnTitulo");
-				
-				oTable.sort(oColumn);
-				oView.byId("tableContaReceber").getBinding("rows").filter(oFilter, "Application");
+			var oFilter = new Filter("Empresa", FilterOperator.EQ, Session.get("EMPRESA_ID"));
+			var oView = this.getView();
+			var oTable = oView.byId("tableContaReceber");
+			
+			oTable.bindRows({ 
+				path: '/ContaRecebers',
+					sorter: {
+						path: 'Titulo'
+				},
+				parameters: {
+					expand: 'CentroCustoDetails,TipoTituloDetails,ParceiroNegocioDetails,PlanoContaDetails'
+				},
+				filters: oFilter
 			});
 		},
 		
 		filtraConta: function(oEvent){
-			var sQuery = oEvent.getParameter("query");
+			var sQuery = oEvent.getParameter("query").toUpperCase();
 			var oFilter1 = new Filter("Empresa", FilterOperator.EQ, Session.get("EMPRESA_ID"));
 			var oFilter2 = new Filter("Titulo", FilterOperator.Contains, sQuery);
 			
